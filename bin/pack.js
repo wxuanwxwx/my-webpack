e9f6e330fd54983d18993b31ae2d002ab4cad888
google110110 <wxuanwxwx@gmail.com>
2021-09-25 17:57:29 +0800

    自定义输出文案；

--- bin/pack.js
+++ bin/pack.js
@@ -3,14 +3,139 @@
 const path = require('path');
 const fs = require('fs');
 const program = require('commander');
+const colors = require('colors-console')
+// https://www.npmjs.com/package/node-console-colors
+const iconvLite = require('iconv-lite');
 const cmd = require('child_process');
 const root = process.cwd();
 const appInfo = require('../package.json');
+const animals = fs.readFileSync(path.join(__dirname, '../log/animals.txt'), 'utf-8').toString().split('===============++++SEPERATOR++++====================\n');
 let ENV = 'dev';
+let newTime = 0;
+let timerId = undefined;
 let configUrl = 'config.dev.json';
 const cdHBuilderXUrl = 'D:\\UNIAPP\\HBuilderX';
 const cdRootUrl = 'cd /';
 const cmdStr = 'cli pack --config';
+const randomAnimal = () => {
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+const startLoad = () => {
+  const P = ["\\", "|", "/", "-"];
+  let x = 0;
+  newTime = Date.parse(new Date());
+  timerId = setInterval(function () {
+    process.stdout.write(`\r ${P[x++]}  以打包:${getTime(newTime)}`);
+    x &= 3;
+  }, 120);
+};
+const endLoad = () => {
+  process.stdout.write(`\r 打包成功，共打包${getTime(newTime)}！`);
+  clearInterval(timerId);
+  newTime = 0;
+  timerId = 0;
+}
+const openCmd = (baseUrl) => {
+  cmd.exec(`${cmdStr} ${baseUrl}`, (error, stdout, stderr) => {
+    endLoad();
+    if (error) {
+      console.log('error：', error);
+    } else if (stdout) {
+      fs.writeFile(path.join(__dirname, '../log/log.txt'), JSON.stringify(stdout), (err) => {
+        console.log('\r 写入成功！', err);
+      });
+      try {
+        console.log('\r stdout：', iconvLite.decode(new Buffer.from(stdout, 'binary'), 'CP936'));
+      } catch (error) {
+        process.stdout.write(`\r stdout：${iconvLite.decode(stdout, 'CP936')}`);
+      }
+    } else if (stderr) {
+      console.log('stderr：', stderr);
+    }
+  })
+}
+
+const getTime = (dateBegin) => {
+  let dateEnd = new Date();
+  let dateDiff = dateEnd.getTime() - dateBegin;
+  let leave1 = dateDiff % (24 * 3600 * 1000)
+  let hours = Math.floor(leave1 / (3600 * 1000))
+  let leave2 = leave1 % (3600 * 1000)
+  let minutes = Math.floor(leave2 / (60 * 1000))
+  let leave3 = leave2 % (60 * 1000)
+  let seconds = Math.round(leave3 / 1000)
+  let str = ``;
+  if (hours) {
+    str += ` ${hours}小时 ${minutes}分钟`;
+  } else if (minutes) {
+    str += ` ${minutes}分钟`;
+  }
+  str += ` ${seconds}秒 `;
+  return str;
+}
+const implement = (baseUrl) => {
+  const animal = randomAnimal();
+  console.log(`\n\n${animal}\n\n`);
+  console.log(` 正在排队打包中...\n`);
+  startLoad();
+  openCmd(baseUrl);
+}
+const start = () => {
+  const baseUrl = path.join(root, configUrl);
+  let isFile = true;
+  try {
+    const statsObj = fs.statSync(baseUrl);
+    isFile = statsObj.isFile();
+  } catch (error) {
+    isFile = false;
+  }
+  if (!isFile) {
+    console.log(colors('red', `${root}目录下没有找到${configUrl}文件`));
+  } else {
+    let template = JSON.parse(fs.readFileSync(baseUrl, 'utf-8'));
+    const formatRoot = root.replace(/\\+/g, '/');
+    if (template.project !== formatRoot) {
+      const formatConfig = path.join(root, '\\manifest.json').replace(/\\+/g, '/');
+      template.project = formatRoot;
+      template.config = formatConfig;
+      fs.writeFile(baseUrl, JSON.stringify(template), (err) => {
+        if (err) {
+          console.log(colors('red', `写入${configUrl}文件失败！`));
+        } else {
+          implement(baseUrl);
+        }
+      });
+    } else {
+      implement(baseUrl);
+    }
+  }
+}
+program
+  .version(appInfo.version)
+  .option('-e, --explain', '这是一个用于uniapp打包Android和IOS的自定义脚本！')
+  .option('-v, --version', appInfo.version)
+  .option('-r, --root', root);
+const uni = program.command('uni');
+uni
+  .command('build [env]')
+  .action(env => {
+    env && (ENV = env);
+    configUrl = `config.${ENV}.json`;
+  })
+  .hook('postAction', () => {
+    if (ENV === 'dev') {
+      console.log(`\n 开始执行打包命令: ${colors('yellow', '开发环境...')}`);
+      start();
+    } else if (ENV === 'prod') {
+      console.log(`\n 开始执行打包命令: ${colors('yellow', '生产环境...')}`);
+      start();
+    } else {
+      console.log(`\n${colors('redBG', '\n 打包命令错误,默认开发环境, pack uni build prod为生产环境,请输入正确命令！')}`);
+    }
+  });
+program.parse(process.argv);
+
+
 const color = {
   'bold': '\x1B[1m%s\x1B[22m',
   'italic': '\x1B[3m%s\x1B[23m',
@@ -36,46 +161,3 @@ const color = {
   'redBG': '\x1B[41m%s\x1B[49m',
   'yellowBG': '\x1B[43m%s\x1B[49m'
 };
-program
-  .version(appInfo.version)
-  .option('-e, --explain', '这是一个用于uniapp打包Android和IOS的自定义脚本！')
-  .option('-v, --version', appInfo.version)
-  .option('-r, --root', root);
-const uni = program.command('uni');
-uni
-  .command('build [env]')
-  .action(env => {
-    env && (ENV = env);
-    configUrl = `config.${ENV}.json`;
-  })
-  .hook('postAction', () => {
-    if (ENV === 'dev') {
-      console.log('开始执行打包命令：开发环境...');
-    } else if (ENV === 'prod') {
-      console.log('开始执行打包命令：生产环境...');
-    } else {
-      console.log(color['redBG'], `打包命令错误,默认开发环境, pack uni build prod为生产环境,请输入正确命令！`);
-    }
-  });
-program.parse(process.argv);
-const start = () => {
-  const baseUrl = path.join(root, configUrl);
-  let isFile = true;
-  try {
-    const statsObj = fs.statSync(baseUrl);
-    isFile = statsObj.isFile();
-  } catch (error) {
-    isFile = false;
-  }
-  if (!isFile) {
-    console.log(color['red'], `${root}目录下没有找到${configUrl}文件`);
-  } else {
-    const template = fs.readFileSync(baseUrl, 'utf-8');
-    cmd.exec(`${cmdStr} ${baseUrl}`, (error, stdout, stderr) => {
-      if(error) console.log('error：', error);
-      else if(stdout) console.log('stdout：', stdout);
-      else if(stderr) console.log('stderr：', stderr);
-    })
-  }
-}
-start();
